6a484bbab5c8abe0051b327b53030283b291f0ef
melsimpson1023@gmail.com <L0v3ch1ld>
2020-11-10 11:38:57 -0500

    Update working
    we got update to work

--- curl-scripts/gemstones/destroy.sh
+++ curl-scripts/gemstones/destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/gemstones"
+URL_PATH="/gemstones/:id"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
--- curl-scripts/gemstones/show.sh
+++ curl-scripts/gemstones/show.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 API="http://localhost:4741"
-URL_PATH="/gemstones"
+URL_PATH="/gemstones/:id"
 
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
-  --header "Content-Type: application/json"
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" 
 
 echo
